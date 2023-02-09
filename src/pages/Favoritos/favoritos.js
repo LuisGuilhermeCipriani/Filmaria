41160172677fb9530c716ea9ad6de4d2b7e39e72
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-02-09 09:38:39 -0300

    Cria páginas

---
+++ src/pages/Favoritos/favoritos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Favoritos(){
+    return(
+        <div>
+            <h2>Página de favoritos</h2>
+        </div>
+    );
+}
+
+export default Favoritos;
---
+++ src/pages/Filmes/filmes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Filmes(){
+    return(
+        <div>
+            <h2>Página de filmes</h2>
+        </div>
+    );
+}
+
+export default Filmes;
