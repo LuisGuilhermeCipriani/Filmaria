976a71d62e5c059b7cc84a5c266d1c9de2d47eed
Luis Guilherme Cipriani <cipriani.luisguilherme@gmail.com>
2023-02-09 11:51:35 -0300

    Lista filmes na página inicial

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-function Home(){
-    return(
-        <div>
-            <h2>Página inicial</h2>
+import api from "../../services/api";
+import './home.css';
+
+function Home() {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        async function loadApi() {
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "782237ee5f2ffb36154141fec759fcfc",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+            setFilmes(response.data.results.slice(0, 10));
+        }
+
+        loadApi();
+    }, []);
+
+    return (
+        <div className="container">
+            <div className="lista_filmes">
+                {filmes.map((item) => {
+                    return (
+                        <article key={item.id}>
+                            <strong>{item.original_title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} alt={item.title} />
+                            <Link to={`/filmes/${item.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     );
 }
